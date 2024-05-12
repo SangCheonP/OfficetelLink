67606da1c21dbc8317ec392c9b80fd250e3135f9
ParkSangCheon <pswlove38@naver.com>
2024-05-12 20:43:44 +0900

    First Commit

---
+++ src/main/java/com/ssafy/offistellink/OffistellinkApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.offistellink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffistellinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffistellinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/offistellink/OffistellinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.offistellink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffistellinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
