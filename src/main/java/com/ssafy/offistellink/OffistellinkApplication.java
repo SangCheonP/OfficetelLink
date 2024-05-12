e08ed4fc6fe7963588d179643ed8949b5c44a8e8
ParkSangCheon <pswlove38@naver.com>
2024-05-12 21:21:56 +0900

    Upload Swagger

--- src/main/java/com/ssafy/offistellink/OffistellinkApplication.java
+++ src/main/java/com/ssafy/offistellink/OffistellinkApplication.java
@@ -3,6 +3,8 @@ package com.ssafy.offistellink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// http://localhost:8080/swagger-ui/index.html
+
 @SpringBootApplication
 public class OffistellinkApplication {
 
